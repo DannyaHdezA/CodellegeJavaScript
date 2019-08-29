642eef5d60ce5f44e8d43657f1b815c70a3fdb91
Dannya Hernandez Angeles <dannya_vi@hotmail.com>
2019-08-29 12:29:35 -0500

    Suma Se crearon estilos con Boostrap-

---
+++ Ejemplo1/app.js
@@ -0,0 +1,10 @@
+let BotonCalcular = document.getElementById('BotonCalcular');
+// let Numero1=document.getElementById('Numero1');
+// let Numero2=document.getElementById('Numero2');
+// let Resultado=document.getElementById('Resultado');
+
+BotonCalcular.addEventListener('click', suma);
+
+function suma() {
+    console.log('X');
+}
