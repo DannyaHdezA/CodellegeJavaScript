9c15ca36f5d586d0603ba3b1c203be87ae0632e7
Dannya Hernandez Angeles <dannya_vi@hotmail.com>
2019-08-30 10:50:36 -0500

    Se realizo la suma de dos numeros ingresados por el usuario.

--- Ejemplo1/app.js
+++ Ejemplo1/app.js
@@ -1,10 +1,29 @@
+//Esta linea crea una variable la cual se enacrga de obtener por medio del Objeto document el elemento button con el id "BotonCalcular"
 let BotonCalcular = document.getElementById('BotonCalcular');
-// let Numero1=document.getElementById('Numero1');
-// let Numero2=document.getElementById('Numero2');
-// let Resultado=document.getElementById('Resultado');
 
+//Por medio del método "addEventListener" se le asigno el evento clic al elemento button y al momento de ejecutar dicho evento se ejecutara la función suma
 BotonCalcular.addEventListener('click', suma);
 
+//Función: bloque de codigo que se ejecuta hasta cuando se manda a llamar o se invoca, mientras no sea invocada no realizara ninguna accion en el código. 
 function suma() {
-    console.log('X');
+
+    // Se obtendra los valores  de los inputs por medio del método "Value"
+    let Numero1 = document.getElementById('Numero1').value;
+    let Numero2 = document.getElementById('Numero2').value;
+
+    //Se reasigna el valor de la variable Numero2 convirtiendolo de string a Numero
+    Numero1 = Number(Numero1);
+    Numero2 = Number(Numero2);
+
+    // Se declara variable "Resultado" y se ininializa con valor '0'
+    let Resultado = 0;
+
+    // Despues d eobtener los datos de los elementos input se realiza el proceso de la suma y se le asigna dicho valor de la variable "Resultado"
+    Resultado = Numero1 + Numero2;
+
+    // Se asigno a la variable "imprimirResultado" el elemento con el ID "Resultado"
+    let imprimirResultado = document.getElementById('Resultado');
+
+    //Por medio del metodo "innerHTML" se asigna el valor de la variable resultado para que se pueda imprimir en el elemento
+    imprimirResultado.innerHTML = Resultado;
 }
